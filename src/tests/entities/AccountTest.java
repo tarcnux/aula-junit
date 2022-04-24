ca6bfe0094f37083cf5e16c0a058d85274da348e
Tarcisio Nunes <github@tarcnux.com.br>
2022-04-24 11:35:42 -0300

    1º teste - Depósito com quantia positiva

---
+++ src/tests/entities/AccountTest.java
@@ -0,0 +1,26 @@
+package tests.entities;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Account;
+import tests.factory.AccountFactory;
+
+class AccountTest {
+
+	@Test
+	void depositShouldIncreaseBalanceAndDiscountFeeWhenPositiveAmount() {
+		
+		// Arrange
+		double amount = 200.0;
+		double expectedValue = 196.00;
+		
+		Account acc = AccountFactory.createEmptyAccount();
+		
+		// Act
+		acc.deposit(amount);
+		
+		// Assertions
+		Assertions.assertEquals(expectedValue, acc.getBalance());		
+	}
+}

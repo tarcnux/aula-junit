f945b39e3224cf969bf3469f5d30302ba3f06621
Tarcisio Nunes <github@tarcnux.com.br>
2022-04-28 00:10:23 -0300

    closes #3

--- src/tests/FinancingTests.java
+++ src/tests/FinancingTests.java
@@ -8,7 +8,7 @@ import entities.Financing;
 class FinancingTests {
 
 	@Test
-	void constructorShouldCreateObjetcWhenValidData() {
+	void constructorShouldCreateObjectWhenValidData() {
 		// Arrange
 		
 		// Action
@@ -18,6 +18,16 @@ class FinancingTests {
 		Assertions.assertEquals(100000.0, f.getTotalAmount());
 		Assertions.assertEquals(2000.0, f.getIncome());
 		Assertions.assertEquals(80, f.getMonths());
+	}
+	
+	@Test
+	void constructorShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		// Arrange
+		
+		// Action
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Financing f = new Financing(100000.0, 2000.0, 20);
+		});
 	}
 
 }
